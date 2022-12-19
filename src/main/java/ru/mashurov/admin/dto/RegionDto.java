a02ec943a9e74029cf537b917f91344576263b72
Dmitriy <lagrik33@gmail.com>
2022-12-19 19:35:38 +0300

    Дто для региона

---
+++ src/main/java/ru/mashurov/admin/dto/RegionDto.java
@@ -0,0 +1,17 @@
+package ru.mashurov.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegionDto {
+
+    private Long code;
+
+    private String name;
+}
