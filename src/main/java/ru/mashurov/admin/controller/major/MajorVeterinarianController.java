d3cc3f7dd69620f1a53309b7314134f2a9b45146
Dmitriy <lagrik33@gmail.com>
2022-12-20 22:51:39 +0300

    Добавлен функционал добавления/изменения/удаления ветеринаров

--- src/main/java/ru/mashurov/admin/controller/major/MajorVeterinarianController.java
+++ src/main/java/ru/mashurov/admin/controller/major/MajorVeterinarianController.java
@@ -7,10 +7,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.mashurov.admin.dto.AdminDto;
 import ru.mashurov.admin.dto.PageResolver;
+import ru.mashurov.admin.dto.VeterinarianCreateDto;
 import ru.mashurov.admin.dto.VeterinarianDto;
 import ru.mashurov.admin.service.AdminService;
 import ru.mashurov.admin.service.VeterinarianService;
@@ -38,13 +42,61 @@ public class MajorVeterinarianController {
 
         final AdminDto admin = adminService.findByUsername(authentication.getName());
 
-        final PageResolver<VeterinarianDto> veterinarianPage
-                = veterinarianService.findAllByClinicId(admin.getClinic().getId(), size, page);
+	    final PageResolver<VeterinarianDto> veterinarianPage
+			    = veterinarianService.findAllByClinicId(admin.getClinic().getId(), size, page);
 
-        model.addAttribute("veterinarians", veterinarianPage.getContent());
-        model.addAttribute("role", authentication.getAuthorities().toArray()[0]);
-        model.addAttribute("pageNumbers", IntStream.range(1, veterinarianPage.getTotalPages()).toArray());
+	    model.addAttribute("veterinarians", veterinarianPage.getContent());
+	    model.addAttribute("role", authentication.getAuthorities().toArray()[0]);
+	    model.addAttribute("pageNumbers", IntStream.range(1, veterinarianPage.getTotalPages()).toArray());
 
-        return "major/veterinarians";
+	    return "major/veterinarians";
     }
+
+	@GetMapping("/veterinarians/{id}/remove")
+	public String remove(@PathVariable final Long id) {
+
+		veterinarianService.deleteById(id);
+
+		return "redirect:/major/veterinarians";
+	}
+
+	@GetMapping("/veterinarians/{id}/update")
+	public String pageUpdate(@PathVariable final Long id, final Model model) {
+
+		final VeterinarianDto veterinarianDto = veterinarianService.findById(id);
+
+		model.addAttribute("veterinarianDto", veterinarianDto);
+
+		return "major/veterinarian";
+	}
+
+	@PostMapping("/veterinarians/{id}/update")
+	public String update(@ModelAttribute final VeterinarianDto veterinarianUpdateDto) {
+
+		veterinarianService.update(veterinarianUpdateDto);
+
+		return "redirect:/major/veterinarians";
+	}
+
+	@GetMapping("/veterinarians/create")
+	public String pageCreate(final Model model) {
+
+		model.addAttribute("veterinarianDto", new VeterinarianCreateDto());
+
+		return "major/veterinarian";
+	}
+
+	@PostMapping("/veterinarians/create")
+	public String create(@ModelAttribute final VeterinarianCreateDto veterinarianCreateDto) {
+
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		final Long clinicId = adminService.findByUsername(authentication.getName()).getClinic().getId();
+
+		veterinarianCreateDto.setClinicId(clinicId);
+
+		veterinarianService.create(veterinarianCreateDto);
+
+		return "redirect:/major/veterinarians";
+	}
 }
--- src/main/java/ru/mashurov/admin/service/VeterinarianService.java
+++ src/main/java/ru/mashurov/admin/service/VeterinarianService.java
@@ -2,21 +2,23 @@ package ru.mashurov.admin.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import ru.mashurov.admin.dto.PageResolver;
+import ru.mashurov.admin.dto.VeterinarianCreateDto;
 import ru.mashurov.admin.dto.VeterinarianDto;
-import ru.mashurov.admin.dto.page.PageResolver;
 
 @Service
 @AllArgsConstructor
 public class VeterinarianService {
 
-    private final WebClient webClient;
+    private final WebClient client;
 
     public PageResolver<VeterinarianDto> findAllByClinicId(
             final Long clinicId, final Integer size, final Integer page
     ) {
 
-        return webClient
+        return client
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path(String.join("/", "api", "clinics", clinicId.toString(), "veterinarians"))
@@ -26,6 +28,60 @@ public class VeterinarianService {
                 )
                 .retrieve()
                 .bodyToMono(PageResolver.class)
+                .block();
+    }
+
+    public VeterinarianDto findById(final Long id) {
+
+        return client
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(String.join("/", "api", "veterinarians", id.toString()))
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(VeterinarianDto.class)
+                .block();
+    }
+
+    public void update(final VeterinarianDto veterinarianUpdateDto) {
+
+        client
+                .patch()
+                .uri(uriBuilder -> uriBuilder
+                        .path(String.join("/", "api", "veterinarians", "update"))
+                        .build()
+                )
+                .body(BodyInserters.fromValue(veterinarianUpdateDto))
+                .retrieve()
+                .toBodilessEntity()
+                .block();
+    }
+
+    public void create(final VeterinarianCreateDto veterinarianCreateDto) {
+
+        client
+                .post()
+                .uri(uriBuilder -> uriBuilder
+                        .path(String.join("/", "api", "veterinarians", "create"))
+                        .build()
+                )
+                .body(BodyInserters.fromValue(veterinarianCreateDto))
+                .retrieve()
+                .toBodilessEntity()
+                .block();
+    }
+
+    public void deleteById(final Long id) {
+
+        client
+                .delete()
+                .uri(uriBuilder -> uriBuilder
+                        .path(String.join("/", "api", "veterinarians", id.toString()))
+                        .build()
+                )
+                .retrieve()
+                .toBodilessEntity()
                 .block();
     }
 }
