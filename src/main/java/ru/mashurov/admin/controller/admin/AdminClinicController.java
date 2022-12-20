fd6009a2c11e314d1fea267215558da1da225689
Dmitriy <lagrik33@gmail.com>
2022-12-20 22:52:54 +0300

    Добавлен функционал для добавления/изменения/удаление клиник

---
+++ src/main/java/ru/mashurov/admin/controller/admin/AdminClinicController.java
@@ -0,0 +1,106 @@
+package ru.mashurov.admin.controller.admin;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.mashurov.admin.dto.AdminDto;
+import ru.mashurov.admin.dto.ClinicDto;
+import ru.mashurov.admin.dto.PageResolver;
+import ru.mashurov.admin.dto.RegionDto;
+import ru.mashurov.admin.service.AdminService;
+import ru.mashurov.admin.service.ClinicService;
+import ru.mashurov.admin.service.RegionService;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+@RequestMapping("/admin")
+public class AdminClinicController {
+
+    private final AdminService adminService;
+
+    private final ClinicService clinicService;
+
+    private final RegionService regionService;
+
+    @GetMapping("/clinics")
+    public String clinics(
+            @RequestParam(defaultValue = "7") final Integer size,
+            @RequestParam(defaultValue = "0") final Integer page,
+            final Model model
+    ) {
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        final AdminDto admin = adminService.findByUsername(authentication.getName());
+
+        final PageResolver<ClinicDto> clinicsPage
+                = clinicService.findAllWithSizeAndPageAndRegion(size, page, admin.getClinic().getRegion().getCode());
+
+        model.addAttribute("clinics", clinicsPage.getContent());
+        model.addAttribute("role", authentication.getAuthorities().toArray()[0]);
+        model.addAttribute("pageNumbers", IntStream.range(1, clinicsPage.getTotalPages() + 1).toArray());
+
+        return "admin/clinics";
+    }
+
+    @GetMapping("/clinics/{id}/remove")
+    public String remove(@PathVariable final Long id) {
+
+        clinicService.deleteById(id);
+
+        return "redirect:/admin/clinics";
+    }
+
+    @GetMapping("/clinics/{id}/update")
+    public String pageUpdate(@PathVariable final Long id, final Model model) {
+
+        final ClinicDto clinicDto = clinicService.findById(id);
+
+        final List<RegionDto> regions = regionService.findAll();
+
+        model.addAttribute("clinicDto", clinicDto);
+        model.addAttribute("regions", regions);
+
+        return "admin/clinic";
+    }
+
+    @PostMapping("/clinics/{id}/update")
+    public String update(@ModelAttribute final ClinicDto clinicDto) {
+
+        clinicService.update(clinicDto);
+
+        return "redirect:/admin/clinics";
+    }
+
+    @GetMapping("/clinics/create")
+    public String pageCreate(final Model model) {
+
+        final List<RegionDto> regions = regionService.findAll();
+
+        model.addAttribute("clinicDto", new ClinicDto());
+        model.addAttribute("regions", regions);
+
+        return "/admin/clinic";
+    }
+
+    @PostMapping("/clinics/create")
+    public String create(@ModelAttribute final ClinicDto clinicDto) {
+
+        clinicService.create(clinicDto);
+
+        return "redirect:/admin/clinics";
+    }
+}
--- src/main/java/ru/mashurov/admin/service/ClinicService.java
+++ src/main/java/ru/mashurov/admin/service/ClinicService.java
@@ -2,6 +2,7 @@ package ru.mashurov.admin.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.mashurov.admin.dto.ClinicDto;
 import ru.mashurov.admin.dto.PageResolver;
@@ -16,17 +17,59 @@ public class ClinicService {
             final Integer size, final Integer page, final Long regionCode
     ) {
 
-        return client
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(String.join("/", "api", "clinics"))
-                        .queryParam("region", regionCode)
-                        .queryParam("size", size)
-                        .queryParam("page", page)
-                        .build()
-                )
-                .retrieve()
-                .bodyToMono(PageResolver.class)
-                .block();
+	    return client
+			    .get()
+			    .uri(uriBuilder -> uriBuilder
+					    .path(String.join("/", "api", "clinics"))
+					    .queryParam("region", regionCode)
+					    .queryParam("size", size)
+					    .queryParam("page", page)
+					    .build()
+			    )
+			    .retrieve()
+			    .bodyToMono(PageResolver.class)
+			    .block();
     }
+
+	public void deleteById(final Long id) {
+
+		client
+				.delete()
+				.uri(String.join("/", "api", "clinics", id.toString()))
+				.retrieve()
+				.toBodilessEntity()
+				.block();
+	}
+
+	public ClinicDto findById(final Long id) {
+
+		return client
+				.get()
+				.uri(String.join("/", "api", "clinics", id.toString()))
+				.retrieve()
+				.bodyToMono(ClinicDto.class)
+				.block();
+	}
+
+	public void update(final ClinicDto clinicDto) {
+
+		client
+				.patch()
+				.uri(String.join("/", "api", "clinics", "update"))
+				.body(BodyInserters.fromValue(clinicDto))
+				.retrieve()
+				.toBodilessEntity()
+				.block();
+	}
+
+	public void create(final ClinicDto clinicDto) {
+
+		client
+				.post()
+				.uri(String.join("/", "api", "clinics", "create"))
+				.body(BodyInserters.fromValue(clinicDto))
+				.retrieve()
+				.toBodilessEntity()
+				.block();
+	}
 }
